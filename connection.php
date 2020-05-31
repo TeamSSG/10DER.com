ca2b2959fc2c813879f011672def61e9a5c7ab2a
Gunjan Kumar <gkr54322@gmail.com>
2020-05-31 20:35:27 +0530

    modified mistakes in login

--- connection.php
+++ connection.php
@@ -5,11 +5,11 @@ $password = "";
 $dbname = "myDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $dbname);
+$conn = new mysqli($servername, $username,$password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
 ?>
