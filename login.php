c24eb6b8b17a3cf2d0ed6a70b03fbba5d338eae1
Gunjan Kumar <gkr54322@gmail.com>
2020-06-01 17:26:14 +0530

    completed login and signup

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ include("connection.php");
 
 <div class="container" id="container">
 	<div class="form-container sign-up-container">
-		<form action="#" method="post">
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 			<h1>Create Account</h1>
 			<!-- <div class="social-container">
 				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
@@ -27,10 +27,26 @@ include("connection.php");
 				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
 			</div> -->
 			<!-- <span>or use your email for registration</span> -->
-			<input type="text" placeholder="Name" name="sname" />
+			<input type="text" placeholder="UserName" name="sname" />
 			<input type="email" placeholder="Email" name="semail"/>
 			<input type="password" placeholder="Password" name="spassword" />
+			<input type="password" placeholder="Confirm Password" name="scpassword" />
+			<input type="text" placeholder="Mobile no" name="smobileno" />
+			
+
+			<select name="category" id="category" style="background-color: #eee;
+				border: none;
+				padding: 12px 15px;
+				margin: 8px 0;
+				width: 100%;
+			">
+			  <option>Choose a category</option>
+			  <option value="provider">provider</option>
+			  <option value="requester">requester</option>
+			  
+			</select>
 			<button name="sign_up">Sign Up</button>
+
 		</form>
 	</div>
 	<div class="form-container sign-in-container">
@@ -42,7 +58,7 @@ include("connection.php");
 				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
 			</div> -->
 			<!-- <span>or use your account</span> -->
-			<input type="email" placeholder="Email" name="lemail"/>
+			<input type="text" placeholder="UserName" name="lname"/>
 			<input type="password" placeholder="Password" name="lpassword"/>
 			
 			<button value="login" name="login">Sign In</button>
@@ -67,23 +83,114 @@ include("connection.php");
 
 <script type="text/javascript" src="js/login.js"></script>
 <?php
-if(isset($_POST['sign_up']) || isset($_POST['login']))
+
+//////////////for signup//////////////////////
+if(isset($_POST['sign_up']))
 {
-	$user=$_POST['lemail'];
-	$password=$_POST['lpassword'];
-	$query1="select * from requester where username='$user' && password='$password'";
-	$query2="select * from provider where username='$user' && password='$password'";
+	$user=$_POST['sname'];
+	$email=$_POST['semail'];
+	$password=$_POST['spassword'];
+	$cpassword=$_POST['scpassword'];
+	$mobile=$_POST['smobileno'];
+	$category=$_POST['category'];
+
+	$pass= password_hash($password, PASSWORD_BCRYPT);
+	$cpass= password_hash($cpassword, PASSWORD_BCRYPT);
+
+	$userquery="select * from $category where username='$user'";
+	$count=$conn->query($userquery);
+
+	if($count->num_rows>0)
+	{
+		echo "<script>alert('username already taken')</script>";
+	}
+	else
+	{
+		if($password===$cpassword)
+		{
+			
+				$insertquery="INSERT INTO $category(username,password,cpassword, mobile, email) VALUES ('$user','$pass','$cpass','$mobile','$email')";
+				$iquery=$conn->query($insertquery);
+				if($iquery)
+				{
+					echo "<script>alert('inserted successfully')</script>";
+					$_SESSION['user']=$user;
+	  				header('Location: 10DER.php');
+				}
+				else
+				{
+					echo "<script>alert('error in insertion')</script>";
+				}
+			
+
+		}
+		else
+		{
+			echo "<script>alert('password did not match')</script>";
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+///////////////////////for login//////////////////////////
+if(isset($_POST['login']))
+{
+	$luser=$_POST['lname'];
+	$lpassword=$_POST['lpassword'];
+	$query1="select * from requester where username='$luser'";
+	$query2="select * from provider where username='$luser'";
 	$result1=$conn->query($query1);
 	$result2=$conn->query($query2);
+	$count1=$result1->num_rows;
+	$count2=$result2->num_rows;
+	echo "$count1";
 
-	if ( $result1->num_rows>0|| $result2->num_rows>0) 
+
+	if ( $count1>0) 
 	{
-		$_SESSION['user']=$user;
-	  header('Location: 10DER.html');
+
+		echo "<script>alert('entered')</script>";
+		$email_pass = $result1->fetch_assoc();
+		$db_pass=$email_pass['password'];
+		echo $email_pass['password'];
+		$pass_decode=password_verify($lpassword,$db_pass);
+		echo "$pass_decode";
+		if($pass_decode)
+		{
+			$_SESSION['user']=$luser;
+	  		header('Location: 10DER.php');
+
+		}
+		else{
+			echo "<script>alert('unable to encode')</script>";
+
+		}
+	}
+	elseif ($count2>0) 
+	{
+	 	$email_pass = $result2->fetch_assoc();
+		$db_pass=$email_pass['password'];
+		$pass_decode=password_verify($lpassword, $db_pass);
+		if($pass_decode)
+		{
+			$_SESSION['user']=$luser;
+	  		header('Location: 10DER.php');
+
+		}
 	} 
 	else 
 	{
-	  echo "please enter correct credentials";
+
+	  echo "<script>alert('please enter correct credentials')</script>";
+	  
 	}
 }
 
